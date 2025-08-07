2b28d43e6b4275e01a1a3596079c12a90787b6e4
Mena Erian <mena3koko2karas1@gmail.com>
2025-08-07 19:01:48 +0300

    Redesign PhoneBook class so that is has  Dictionary<string, int> Entries with the same functionality of the class

---
+++ Lab/Helper.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public static class Helper
+    {
+        public static int GetIntFromUserUsingTryParse(string dataName)
+        {
+            int number = 0;
+            do
+            {
+                Console.Write($"Please Enter {dataName}: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out number));
+            return number;
+        }
+        public static int GetIntFromUser(string massageToUser, out int number, bool isMainMsg = true  )
+        {
+             number = 0;
+            switch (isMainMsg)
+            {
+                case true:
+                    do
+                    {
+                        Console.Write(massageToUser);
+                    }
+                    while (!int.TryParse(Console.ReadLine(), out number));
+                    break;
+                case false:
+                    do
+                    {
+                        Console.Write($"Please Enter the {massageToUser}: ");
+                    }
+                    while (!int.TryParse(Console.ReadLine(), out number));
+                    break;
+            }
+
+            return number;
+        }
+
+        public static int GetIntFromUser(string massageToUser, bool isMainMsg = true)
+        {
+            int number = 0;
+            switch (isMainMsg)
+            {
+                case true:
+                    do
+                    {
+                        Console.Write(massageToUser);
+                    }
+                    while (!int.TryParse(Console.ReadLine(), out number));
+                    break;
+                case false:
+                    do
+                    {
+                        Console.Write($"Please Enter the {massageToUser}: ");
+                    }
+                    while (!int.TryParse(Console.ReadLine(), out number));
+                    break;
+            }
+
+            return number;
+        }
+        public static decimal GetDecimalFromUser(string massageToUser, bool isMainMsg = true)
+        {
+            decimal number = 0;
+            switch (isMainMsg)
+            {
+                case true:
+                    do
+                    {
+                        Console.Write(massageToUser);
+                    }
+                    while (!decimal.TryParse(Console.ReadLine(), out number));
+                    break;
+                case false:
+                    do
+                    {
+                        Console.WriteLine($"Please Enter the {massageToUser}: ");
+                    }
+                    while (!decimal.TryParse(Console.ReadLine(), out number));
+                    break;
+            }
+
+            return number;
+        }
+        public static string GetStringFromUser(string dataName)
+        {
+            string str = string.Empty;
+            do
+            {
+                Console.Write($"Please Enter the {dataName}: ");
+                str = Console.ReadLine() ?? string.Empty;
+            }
+            while (str == string.Empty || int.TryParse(str, out _));
+
+            return str;
+        }
+        public static char GetCharFromUser(string msg, bool isMainMsg = false)
+        {
+            char character = default(char);
+            bool isParsed = false;
+            if (isMainMsg)
+            {
+                do
+                {
+                    Console.Write(msg);
+                    isParsed = char.TryParse(Console.ReadLine(), out character);
+                }
+                while (!(isParsed && !int.TryParse(character.ToString(), out _)));
+            }
+            else
+            {
+                do
+                {
+                    Console.Write($"Please Enter the {msg}: ");
+                    isParsed = char.TryParse(Console.ReadLine(), out character);
+                }
+                while (!(isParsed && !int.TryParse(character.ToString(), out _)));
+            }
+
+            return character;
+        }
+    }
+}
---
+++ Lab/MyPhoneBook/PhoneBook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.MyPhoneBook
+{
+
+
+    internal class PhoneBook : Dictionary<string, int>
+    {
+        //internal class Person
+        //{
+        //    public int Number { get; set; }
+        //    public string? Name { get; set; }
+        //    public override string ToString()
+        //    {
+        //        return $"Name: {Name}, Number: {Number}";
+        //    }
+        //}
+
+
+
+        #region Fields
+
+        //private Person[] persons;
+        //private int currentIndex = -1;
+        #endregion
+
+        #region Indexer
+        public string this[int userNumber]
+        {
+            get
+            {
+                foreach (KeyValuePair<string, int> user in this)
+                    if (user.Value == userNumber) return user.Key;
+
+                return "USER NOT FUONDED";
+            }
+            set
+            {
+                if (this.TryAdd(value, userNumber)) return;
+                Remove(value);
+                Add(value, userNumber);
+            }
+        }
+        #endregion
+
+        
+    }
+}
